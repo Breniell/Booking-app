c3885c88ed11346faa1f6fbee26f293ba3f15575
Breniell <breniellkouda@gmail.com>
2025-03-09 15:55:03 +0100

    liaison du front et back en modifiant Base_URL

--- booking-app-frontend/src/lib/api.ts
+++ booking-app-frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://booking-app-backend-2tla.onrender.com',
 });
 
 // Request Interceptor
