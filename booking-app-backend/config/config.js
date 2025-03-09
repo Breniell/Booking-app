9744fc4b70506e850e2b194fc58ecf4588b0d926
Breniell <breniellkouda@gmail.com>
2025-03-09 06:24:46 +0100

    modifications base de donnees en postgreSQL

--- booking-app-backend/config/config.js
+++ booking-app-backend/config/config.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
 
 module.exports = {
-    "development": {
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": "mysql",
-      logging: false
-    },
-    "production": {  //Example - adjust for your prod environment
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
-      "host": process.env.DB_HOST,
-      "dialect": "mysql",
-      logging: false
-    }
-  };
-  
+  "development": {
+    "username": process.env.DB_USERNAME, // Exemple: postgres
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,       // Exemple: booking_app_db
+    "host": process.env.DB_HOST,           // Exemple: your-db-host.render.com
+    "port": process.env.DB_PORT || 5432,
+    "dialect": "postgres",                 // Passage à Postgres
+    "logging": false
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT || 5432,
+    "dialect": "postgres",
+    "logging": false
+  }
+};
