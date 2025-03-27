d0b31cc7927d5288d047114b18237d5eeb6a5b18
Breniell <breniellkouda@gmail.com>
2025-03-27 08:03:07 +0100

    correction de la redirection choix au visiteur de se connecter ou sinscrire avant datteindre les pages securisees

--- booking-app-frontend/src/lib/api.ts
+++ booking-app-frontend/src/lib/api.ts
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://booking-app-backend-2tla.onrender.com/api',
+  
+  // 'http://localhost:5000/api'
   // Vous pouvez activer d'autres options ici si besoin
 });
 
